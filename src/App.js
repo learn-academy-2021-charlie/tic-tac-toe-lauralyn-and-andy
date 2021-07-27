b0650a937dd088ec0a4c64ffa66513f42cf54588
Andy19936 <andy19936@gmail.com>
2021-07-27 13:36:00 -0700

    winning seq

--- src/App.js
+++ src/App.js
@@ -27,12 +27,33 @@ class App extends Component{
   7) 0, 4, 8
   8) 2, 4, 6
   */
+  winSequence = () => {
+    const { board} = this.state
+    if((board[0] === board[1] && board[0] === board[2])
+      || (board[3] === board[4] && board[3] === board[5])
+      || (board[6] === board[7] && board[6] === board[8])
+      || (board[0] === board[3] && board[0] === board[6])
+      || (board[1] === board[4] && board[1] === board[7])
+      || (board[2] === board[5] && board[2] === board[8])
+      || (board[0] === board[4] && board[0] === board[8])
+      || (board[2] === board[4] && board[2] === board[6])
+      ){ 
+        return true
+      } else {
+        return false
+      }
+      
+
+  }
+
   markSquare = (player, i) => {
     const { board} = this.state
 
     let square = board[i]
     if(square === ""){
       board[i] = player
+      if (this.winSequence)
+      {console.log(player)}
       this.setState({
         player1: !this.state.player1
       })
